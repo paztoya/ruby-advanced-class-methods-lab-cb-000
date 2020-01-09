4e2bfa779c8ec0f6472761454578ac1230102c09
Daniel Paz <daniel@paztoya.com>
2020-01-09 04:24:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  self.class.all << self
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
   end
 
   def self.create
@@ -32,16 +39,30 @@ class Song
     self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
   end
 
+  def self.alphabetical
+    songs = self.all.sort_by {|song| song.name}
+    songs.uniq {|song| song}
+  end
+
   def self.new_from_filename(filename)
-    file = filename.sub(".mp3", "").split(" - ")
+    file = filename.sub(".mp3","").split(" - ")
     song = self.new
     song.name = file[1]
     song.artist_name = file[0]
     song
   end
 
+  def self.create_from_filename(filename)
+    file = filename.sub(".mp3","").split(" - ")
+    song = self.new
+    song.name = file[1]
+    song.artist_name = file[0]
+    song.save
+    song
+  end
+
   def self.destroy_all
     self.all.clear
   end
-  
+
 end
